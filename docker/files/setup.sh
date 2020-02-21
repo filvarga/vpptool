b25f09d694267a5dc5f266058c76a6228ba1522e
Filip Varga <filipvarga89@gmail.com>
2020-02-21 09:51:03 +0100

    solved building problem because of missing dependencie

--- docker/files/setup.sh
+++ docker/files/setup.sh
@@ -6,6 +6,11 @@ git pull
 git checkout $CID -b build
 
 UNATTENDED=y apt-get update && apt-get upgrade -y
+
+# INFO: missing dependencie in make install-dep !!!
+# required by install-ext-deps
+UNATTENDED=y apt install libssl-dev -y
+
 UNATTENDED=y make install-dep
 UNATTENDED=y make install-ext-deps
 rm -rf /var/lib/apt/lists
